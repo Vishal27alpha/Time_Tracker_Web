82103cc1dfe0753f8f712dc1895c213862ceef9f
Vishal27alpha <120443254+Vishal27alpha@users.noreply.github.com>
2025-11-01 01:11:29 +0530

    Fix: adjusted layout and overlapping text in AddedTask component

--- src/components/addedTask/AddedTask.component.jsx
+++ src/components/addedTask/AddedTask.component.jsx
@@ -1,134 +1,147 @@
 import React from 'react';
-import {useDispatch} from 'react-redux';
-import {removeTask,pauseTask,startTask,completeTask} from '../../redux/task/Task.action'
+import { useDispatch } from 'react-redux';
+import { removeTask, pauseTask, startTask, completeTask } from '../../redux/task/Task.action';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
 import CancelIcon from '@material-ui/icons/Cancel';
 import Tooltip from '@material-ui/core/Tooltip';
-const AddedTask=({completed,bgColor,taskName,paused,percentage,type,time})=>{
-    //console.log(taskName+' '+paused)
-    const dispatch=useDispatch();
-    let backgrouColorForStart="#e0e0de";
-    if(paused===false){
-        backgrouColorForStart="#c1edb4";
+
+const AddedTask = ({ completed, bgColor, taskName, paused, percentage, type, time }) => {
+    const dispatch = useDispatch();
+
+    let backgrouColorForStart = "#e0e0de";
+    if (paused === false) {
+        backgrouColorForStart = "#c1edb4";
     }
-    const containerStyle={
-        position:'relative',
-        height:'15%',
-        width:'100%',
+
+    const containerStyle = {
+        position: 'relative',
+        height: '15%',
+        width: '100%',
         backgroundColor: backgrouColorForStart,
         borderRadius: '10px',
-        'margin-top':5,
-        display:'flex',
-    }
+        marginTop: 5,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        overflow: 'hidden',
+    };
 
     const fillerStyle = {
         height: '100%',
         width: `${percentage}%`,
         backgroundColor: `${bgColor}`,
-        textAlign: 'right',
         borderRadius: '5px',
         transition: 'width 1s ease-in-out',
-      }
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: 0,
+    };
 
-      const labelStyle = {
-        position:'absolute',
-        top:'1vh',
-        left:'-3vw',
-        color: 'black',
+    const textContainerStyle = {
+        position: 'relative',
+        zIndex: 2,
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '0 1vw',
         fontWeight: 'bold',
-        'z-index':-1,
-        width:'10vw',
-        
-      }
+        color: 'black',
+    };
 
-      const taskContentStyle={
-        position:'absolute',
-        display:'flex',
-        right:'50%',
-        top:'10%',
-        textAlign:'right',
+    const taskNameStyle = {
+        flex: 1,
+        textAlign: 'left',
+        fontWeight: 'bold',
+    };
 
-        'font-weight':'bold',
-      }
-      const iconsListStyle={
-        position:'absolute',
-        top:'0.5vh',
-        display:'flex',
-        left:'1vw',
-        'padding-left':'7.5vw',
-        textAlign: 'right'
-      }
-      const taskNameStyle={
-          position:'absolute',
-          top:'1vh',
-          color:'black',
-          right:'1.9vw',
-          'text-align':'left',
-          width:'11vw',
-          'margin-left':'10%',
-      }
-      
+    const timePercentStyle = {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: '0.5rem',
+        fontWeight: 'bold',
+    };
 
-    return(
+    const iconsListStyle = {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: '0.5rem',
+        marginLeft: '1vw',
+    };
+
+    const timeString = `${time[0]}${time[1]}:${time[2]}${time[3]}:${time[4]}${time[5]}`;
+
+    return (
         <Tooltip title={taskName.toUpperCase()} placement="right-start" arrow>
-        <div className='taskMainContainer' style={containerStyle}>
-            <div className='contentContainer' style={fillerStyle}>
-               
-            </div>
-            <div className='taskContent' style={taskContentStyle}>
-                <span style={labelStyle}>{`${time[0]+''+time[1]+':'+time[2]+''+time[3]+':'+time[4]+''+time[5]}  -  ${percentage}%`}</span>
-                <div className='taskName' style={taskNameStyle}>
-                    {`${taskName.toUpperCase().slice(0,14)}${taskName.length>14?'...':''}`}
-                </div>
-                {completed && type!=='remainder'?'':
-            
-                
-                <div className='iconsList' style={iconsListStyle}>
-                    {paused
-                        ?
-                            type!=='remainder'
-                            ?
-                                <Tooltip title={`Start ${taskName.toUpperCase()}`}  arrow>
-                                    <div className='playButton'>
-                                        <PlayCircleOutlineIcon key={`${taskName}-playBtn`} onClick={()=>dispatch(startTask(taskName))}/>
+            <div className="taskMainContainer" style={containerStyle}>
+                <div className="contentContainer" style={fillerStyle}></div>
+
+                <div style={textContainerStyle}>
+                    <div className="taskName" style={taskNameStyle}>
+                        {`${taskName.toUpperCase().slice(0, 14)}${taskName.length > 14 ? '...' : ''}`}
+                    </div>
+
+                    <div style={timePercentStyle}>
+                        <span>{timeString}</span>
+                        <span>-</span>
+                        <span>{`${percentage}%`}</span>
+                    </div>
+
+                    {!completed && type !== 'remainder' && (
+                        <div className="iconsList" style={iconsListStyle}>
+                            {paused ? (
+                                type !== 'remainder' && (
+                                    <Tooltip title={`Start ${taskName.toUpperCase()}`} arrow>
+                                        <div className="playButton">
+                                            <PlayCircleOutlineIcon
+                                                key={`${taskName}-playBtn`}
+                                                onClick={() => dispatch(startTask(taskName))}
+                                            />
+                                        </div>
+                                    </Tooltip>
+                                )
+                            ) : (
+                                type !== 'remainder' && (
+                                    <Tooltip title={`Pause ${taskName.toUpperCase()}`} arrow>
+                                        <div className="pauseButton">
+                                            <PauseCircleOutlineIcon
+                                                key={`${taskName}-pauseBtn`}
+                                                onClick={() => dispatch(pauseTask(taskName))}
+                                            />
+                                        </div>
+                                    </Tooltip>
+                                )
+                            )}
+
+                            {type === 'task' && (
+                                <Tooltip title={`Mark Complete ${taskName.toUpperCase()}`} arrow>
+                                    <div className="completedButton">
+                                        <DoneOutlineIcon onClick={() => dispatch(completeTask(taskName))} />
                                     </div>
                                 </Tooltip>
-                            :''
-                            :type!=='remainder'
-                            ?
-                                <Tooltip title={`Pause ${taskName.toUpperCase()}`}  arrow>
-                                    <div className='playButton'>
-                                        <PauseCircleOutlineIcon key={`${taskName}-pauseBtn`} onClick={()=>dispatch(pauseTask(taskName))}/>
+                            )}
+
+                            {type !== 'break' && (percentage <= 0 || type === 'remainder') && (
+                                <Tooltip title={`Remove ${taskName.toUpperCase()}`} arrow>
+                                    <div className="removeButton">
+                                        <CancelIcon
+                                            key={`${taskName}-cancelBtn`}
+                                            onClick={() => dispatch(removeTask(taskName))}
+                                        />
                                     </div>
                                 </Tooltip>
-                            :''
-                    }
-                    
-                    {type!=='task'?'':
-                        <Tooltip title={`Mark Complete ${taskName.toUpperCase()}`}  arrow>
-                            <div className='completedButton'>
-                                <DoneOutlineIcon onClick={()=>dispatch(completeTask(taskName))}/>
-                            </div>
-                        </Tooltip>
-                    }
-                    {type==='break'?'':percentage>0 && type!=='remainder'?'':
-                        <Tooltip title={`Remove ${taskName.toUpperCase()}`}  arrow>
-                            <div className='removeButton'>
-                                <CancelIcon key={`${taskName}-cancelBtn`} onClick={()=>dispatch(removeTask(taskName))}/>
-                            </div>
-                        </Tooltip>
-                        
-                    }
-                    
-                     
+                            )}
+                        </div>
+                    )}
                 </div>
-                }
             </div>
-        </div>
         </Tooltip>
-    )
-}
+    );
+};
 
 export default React.memo(AddedTask);
